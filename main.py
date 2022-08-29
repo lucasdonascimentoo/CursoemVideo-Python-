2590342eb6c36dec0cfd7aa9d9c61d52aad49fbe
Lucas do Nascimento <63742698+Balthord@users.noreply.github.com>
2022-08-29 08:27:07 -0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-palavras=('aprender','programar','linguagem','python','curso','gratis','estudar','praticar','trabalhar','mercado','programador','futuro')
-
-for p in palavras:
-  print(f'\nNas palavras {p.upper()} temos ',end='')
-  for letras in p:
-    if letras.lower() in 'aeiou':
-      print(letras,end='')
